cc1c971d7a9e0076ac6560aa8211b6910be910db
Stef-00012 <admin@stefdp.com>
2025-04-20 17:56:46 +0200

    add project desc & gh url

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,36 +4,40 @@ export const projects: Array<Project> = [
 	{
 		name: "Receiptify",
 		url: "https://receiptify.stefdp.com",
-        description: ""
+        description: "Receiptify is a website that allows you to convert your top tracks (on last.fm or Spotify). It fetches your top tracks and renders them in a way to look like a receipt.",
+        source: "https://github.com/stef-00012/receiptify"
 	},
 	{
 		name: "Zipline Webstore Extension",
 		url: "https://chromewebstore.google.com/detail/zipline-upload/nckdinonilcnlmjipgggnejkpdldjmjn",
-        description: ""
+        description: "Zipline Uploads allow you to upload your right clicked files or shorten your right clicked URLs to your selfhosted instance of Zipline without having to download the file to your PC and upload it manually.",
+        source: "https://github.com/Stef-00012/Zipline-Upload-Extension"
 	},
 	{
 		name: "Zipline Android App",
 		url: "https://github.com/stef-00012/zipline-android-app/releases/latest",
-        description: ""
+        description: "An Android app to manage your self-hosted zipline V4 instance.",
+        source: "https://github.com/Stef-00012/Zipline-Android-App"
 	},
 	{
 		name: "My Discord Bot - UserApps",
 		url: "https://discord.com/oauth2/authorize?client_id=1223221223685886032",
-        description: ""
+        description: "userApps is a multi-purpose Discord bot iinstalled on your user account so that it can be used everywhere.",
+        source: "https://github.com/Stef-00012/userApps"
 	},
 	{
 		name: "Homepage Dashboard",
 		url: "https://dash.stefdp.com",
-        description: ""
+        description: "A dashboard with most of my self-hosted services."
 	},
 	{
 		name: "My API",
 		url: "https://docs.stefdp.com",
-        description: ""
-	},
+        description: "An API is used to communicate with my apps or to get data about them (Not really mainatined anymore)."
+    },
 	{
 		name: "My Discord Bot - GattinhosBot",
 		url: "https://gattinhosbot.is-a.dev",
-        description: ""
+        description: "A multi-purpose Discord bot (Not really maintained anymore).",
 	},
 ];
