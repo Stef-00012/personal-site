9c437cb246864661f5b9d76f5aa1a3802e4c2b21
Stef-00012 <admin@stefdp.com>
2025-04-20 16:32:11 +0200

    divide repos & projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,6 +7,7 @@ import { projects } from "@/data/projects"
 import Project from "@/components/projectCard"
 
 export default function Projects() {
+    const [topRepos, setTopRepos] = useState<ProjectType[]>([])
     const [projectList, setProjectList] = useState<ProjectType[]>(projects)
 
     useEffect(() => {
@@ -20,7 +21,7 @@ export default function Projects() {
                 git: repo
             }))
 
-            setProjectList(prevProjects => [...projectRepos, ...prevProjects])
+            setTopRepos(projectRepos)
         }
 
         fetchTopRepos()
@@ -28,22 +29,22 @@ export default function Projects() {
 
     return (
         <div className="p-10">
-            <h1 className="font-mono ml-5 my-2 font-bold"> // Top GitHub Repositories</h1>
+            <h1 className="font-mono ml-5 my-2 font-bold"> &#47;&#47; Top GitHub Repositories</h1>
             <div className="min-h-screen sm:mx-4 flex justify-center items-center ">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {projectList.map((project, index) => (
+                    {topRepos.map((repo) => (
                         <Project
-                            key={project.name}
-                            project={project}
+                            key={repo.git?.id}
+                            project={repo}
                         />
                     ))}
                 </div>
             </div>
             
-            <h1 className="font-mono ml-5 my-2 font-bold"> // Other Projects</h1>
+            <h1 className="font-mono ml-5 my-2 font-bold"> &#47;&#47; Other Projects</h1>
             <div className="min-h-screen sm:mx-4 flex justify-center items-center ">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {projectList.map((project, index) => (
+                    {projectList.map((project) => (
                         <Project
                             key={project.name}
                             project={project}
