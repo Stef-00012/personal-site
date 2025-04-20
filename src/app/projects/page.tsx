d625beabb62f2f3d0631907ccf027e323b0db7d9
Stef-00012 <admin@stefdp.com>
2025-04-20 16:36:34 +0200

    add template loading for gh repos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -30,16 +30,20 @@ export default function Projects() {
     return (
         <div className="p-10">
             <h1 className="font-mono my-4 font-bold sm:mx-4 flex justify-center items-center mb-10"> &#47;&#47; Top GitHub Repositories</h1>
-            <div className="sm:mx-4 flex justify-center items-center ">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {topRepos.map((repo) => (
-                        <Project
-                            key={repo.git?.id}
-                            project={repo}
-                        />
-                    ))}
+            {topRepos.length > 0 ? (
+                <div className="sm:mx-4 flex justify-center items-center ">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {topRepos.map((repo) => (
+                            <Project
+                                key={repo.git?.id}
+                                project={repo}
+                            />
+                        ))}
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <p>Some loading</p>
+            )}
             
             <h1 className="font-mono my-4 font-bold sm:mx-4 flex justify-center items-center mt-10"> &#47;&#47; Other Projects</h1>
             <div className="sm:mx-4 flex justify-center items-center ">
