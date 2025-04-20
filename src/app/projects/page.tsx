258e0e0d7abe4aeec6363a6107d62419a9ad4f12
orangci <orangc@proton.me>
2025-04-20 13:42:26 +0300

    top repo info

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,30 @@
 export default function Projects() {
     return (
-        <p>hi</p>
+        <div className="min-h-screen flex justify-center items-center max-w-7xl">
+            <div className="flex gap-4 flex-wrap justify-center">
+
+                {/* Individual Project Card Template */}
+                <div className="card card-lg bg-base-100 shadow shadow-base-100">
+                    <div className="card-body">
+                        <p className="card-title">Project Title</p>
+                        {/* for this div element below, only show it for top repos */}
+                        <div className="top-repo-info">
+                            <p>
+                                <p className="me-1"><span className="-mb-1 icon-[tabler--star] size-5"></span><a href="github.com/stef/project/stargazers">3</a></p>
+                                <p className="me-1"><span className="-mb-1 icon-[tabler--git-fork] size-5"></span><a href="github.com/stef/project/forks">3</a></p>
+                                <p className="me-1"><span className="-mb-1 icon-[tabler--circle-dot] size-5"></span><a href="github.com/stef/project/issues">3</a></p>
+                                <p className="me-1"><span className="-mb-1 icon-[tabler--eye] size-5"></span><a href="github.com/stef/project/watchers">3</a></p>
+                            </p> <br />
+                            <p className="-mt-2 text-base-content/70 italic">License: GNU AGPLv3</p>
+                        </div>
+                        About this project...
+                        <div className="card-actions mt-4 -mb-4">
+                            <a href="https://example.com" className="btn btn-soft btn-accent flex items-center"><span className="icon-[tabler--link] size-5"></span>Project Link</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
