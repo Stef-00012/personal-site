f603f05757cc0dde48cf2e9931ae40255d887a12
orangci <orangc@proton.me>
2025-04-21 07:59:19 +0300

    style notfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,8 @@
 export default function NotFound() {
     return (
-        <p>Not Found</p>
+        <div className="justify-center items-center flex min-h-screen flex-col">
+            <h1 className="text-4xl">404 ~ We couldn't find the page you were looking for.</h1>
+            <a href="/" className="mt-2 text-accent hover:text-accent-content hover:bg-accent px-2">Return to the main page?</a>
+            </div>
     )
 }
