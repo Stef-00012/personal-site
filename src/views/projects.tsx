ed3c40216bf819e852dc88cdd356c45ae743c25d
orangci <orangc@proton.me>
2025-04-23 16:37:29 +0300

    feat: add back button to projects and rabbit page

--- src/views/projects.tsx
+++ src/views/projects.tsx
@@ -9,7 +9,10 @@ interface Props {
 export default function Projects({ topRepos }: Props) {
     return (
         <div className="p-10">
-            <h1 className="font-mono my-4 font-bold sm:mx-4 flex justify-center items-center mb-10"> &#47;&#47; Top GitHub Repositories</h1>
+            <h1 className="my-4 font-bold sm:mx-4 flex justify-between text-4xl items-center mb-10">
+                <span>Projects</span>
+                <a href="#home" className='btn btn-soft btn-accent'><span className='icon-[tabler--arrow-back] size-5'></span> Back</a>
+                </h1>
             {topRepos.length > 0 ? (
                 <div className="sm:mx-4 flex justify-center items-center ">
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
--- src/views/rabbit.tsx
+++ src/views/rabbit.tsx
@@ -5,7 +5,10 @@ export default function Rabbit() {
     return (
 
         <div className="p-10">
-            <h1 className="my-4 font-bold sm:mx-4 flex justify-center text-4xl items-center mb-10">Pallino the Rabbit</h1>
+            <h1 className="my-4 font-bold sm:mx-4 flex justify-between text-4xl items-center mb-10">
+                <span>Pallino the Rabbit</span>
+                <a href="#home" className='btn btn-soft btn-accent'><span className='icon-[tabler--arrow-back] size-5'></span> Back</a>
+                </h1>
             <div className="sm:mx-4 flex justify-center items-center ">
                 <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
                     {[...new Array(rabbitImagesCount).keys()].map((i) => (
