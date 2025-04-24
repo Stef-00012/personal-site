28dd4cd6ffc803b536f33361dffd86e4845033cf
orangci <orangc@proton.me>
2025-04-24 14:39:52 +0300

    home button

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -4,6 +4,12 @@ export default function About() {
     return (
         <div className="flex justify-center items-center min-h-screen px-4">
             <div className="bg-base-100 shadow-xl rounded-box p-6 max-w-2xl w-full">
+            <h1 className="my-4 font-bold sm:mx-4 flex justify-between text-2xl items-center mb-10">
+				<span>About</span>
+				<a href="#home" className="btn btn-soft btn-accent">
+					<span className="icon-[tabler--arrow-back] size-5" /> Back
+				</a>
+			</h1>
                 <div className="text-base-content mockup-code bg-base-200 p-4">
                     <pre>
                         Bonjour! I'm Steve, a <em>homosexually flamboyant</em> fellow thriving in the romantic streets of Paris.
