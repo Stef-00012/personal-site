98f810c890ddc4009eb5d2073b7bb6c78bbdcad2
Stef-00012 <admin@stefdp.com>
2025-04-30 20:03:22 +0200

    fix build error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -19,7 +19,7 @@ export default function GlobalError({
 		<html lang="en" data-theme="catppuccin-macchiato" className="bg-base-300">
 			<body>
 				<h2>Something went wrong! (global-error.tsx)</h2>
-				<h2>This occurs when an error is thrown in the root layout and so can't be handled by any error boundary (aka any error thrown by components directly in layout.tsx)</h2>
+				<h2>This occurs when an error is thrown in the root layout and so can&apos;t be handled by any error boundary (aka any error thrown by components directly in layout.tsx)</h2>
 				<button
 					className="btn btn-success"
 					type="button"
