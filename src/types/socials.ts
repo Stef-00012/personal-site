99701fdd044806769b3970af793ef433740a4a56
Stef-00012 <admin@stefdp.com>
2025-05-12 18:58:57 +0200

    add ko-fi, rm useless types

--- src/types/socials.ts
+++ src/types/socials.ts
@@ -1,7 +1,5 @@
 export interface Social {
-	tooltipStyle: React.CSSProperties; // could be unused
 	type: "social" | "mail";
-	tooltipBorder: string; // could be unused
 	username: string;
 	tooltip: string;
 	color: string;
